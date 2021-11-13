d8f1d02eb815b8d4a1efc7e93080bf0464549620
Yash Pachorkar <90203559+yashpachorkar@users.noreply.github.com>
2021-11-13 00:19:12 -0500

    Update pythontasks2LogisticRegression.py

--- Home_work_2/pythontasks2LogisticRegression.py
+++ Home_work_2/pythontasks2LogisticRegression.py
@@ -8,11 +8,12 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
  
 #Loading the dataset
-df = pd.read_csv(r'C:\AnnacondaProjects\Anna Projects\Homework 2\red_wine.csv')
+dataset = pd.read_csv(r'C:\AnnacondaProjects\Anna Projects\Homework 2\red_wine.csv')
 
-
-X = df.iloc[:,:-1]
-y = df.iloc[:,-1]
+# data = load_breast_cancer(as_frame = True)
+# df = data.frame
+X = dataset.iloc[:,:-1]
+y = dataset.iloc[:,-1]
  
 #Implementing cross validation
  
@@ -42,3 +43,53 @@ clf = LogisticRegression(solver="liblinear", random_state=0).fit(X, y)
 roc_auc_score(y, clf.predict_proba(X)[:, 1])
 roc_auc_score(y, clf.decision_function(X))
 
+
+/
+*********************************************************************************************************************************************************************
+/
+
+
+# Logistic regression
+#Importing required libraries
+import numpy as np
+
+import pandas as pd
+from sklearn.model_selection import KFold 
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+ 
+#Loading the dataset
+dataset = pd.read_csv(r'C:\AnnacondaProjects\Anna Projects\Homework 2\white_wine.csv')
+
+# data = load_breast_cancer(as_frame = True)
+# df = data.frame
+X = dataset.iloc[:,:-1]
+y = dataset.iloc[:,-1]
+ 
+#Implementing cross validation
+ 
+k = 10
+kf = KFold(n_splits=k, random_state=None)
+model = LogisticRegression(solver= 'liblinear')
+ 
+acc_score = []
+ 
+for train_index , test_index in kf.split(X):
+    X_train , X_test = X.iloc[train_index,:],X.iloc[test_index,:]
+    y_train , y_test = y[train_index] , y[test_index]
+     
+    model.fit(X_train,y_train)
+    pred_values = model.predict(X_test)
+     
+    acc = accuracy_score(pred_values , y_test)
+    acc_score.append(acc)
+     
+avg_acc_score = sum(acc_score)/k*100
+ 
+print('accuracy of each fold - {}'.format(acc_score))
+print('Avg accuracy : {}'.format(avg_acc_score))
+
+from sklearn.metrics import roc_auc_score
+clf = LogisticRegression(solver="liblinear", random_state=0).fit(X, y)
+roc_auc_score(y, clf.predict_proba(X)[:, 1])
+roc_auc_score(y, clf.decision_function(X))
