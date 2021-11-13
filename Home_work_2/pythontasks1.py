d0db218823edd3113622e56a9a075987268c9fa2
Yash Pachorkar <90203559+yashpachorkar@users.noreply.github.com>
2021-11-13 00:04:36 -0500

    Update pythontasks1.py

--- Home_work_2/pythontasks1.py
+++ Home_work_2/pythontasks1.py
@@ -4,9 +4,9 @@ import numpy as np
 import pandas as pd
 from pandas_profiling import ProfileReport
 
-df = pd.read_csv(r'C:\AnnacondaProjects\Anna Projects\Homework 2\red_wine.csv')
+profiledf = pd.read_csv(r'C:\AnnacondaProjects\Anna Projects\Homework 2\red_wine.csv')
 
-profile = ProfileReport(df)
+profile = ProfileReport(profiledf)
 profile
 profile.to_notebook_iframe()
 
